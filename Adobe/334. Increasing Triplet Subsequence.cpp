a8fd916c2e97aaf3f2b3a686de1e94648192028c
Harsh Chandwani <harshchandwani50028@gmail.com>
2023-01-12 19:14:42 +0530

    day 2 , 1  question of Adobe

---
+++ Adobe/334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+    int c1 = INT_MAX, c2 = INT_MAX;
+    for (int x : nums) {
+        if (x <= c1) {
+            c1 = x;           
+        } else if (x <= c2) { 
+            c2 = x;           
+        } else {              
+            return true;      
+        }
+    }
+    return false;
+}
+};
